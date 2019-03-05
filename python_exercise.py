63d8634ae79af7330f4830d891125ff7969869ae
david <davidbzyk@yahoo.com>
2019-03-04 21:28:05 -0500

    corrected python_exercise.py to include api calls and adjust spreadsheet based on internal ids

--- python_exercise.py
+++ python_exercise.py
@@ -1,9 +1,30 @@
 import pandas as pd
+import requests
+from pandas.io.json import json_normalize
+import json
+import csv
+
+# Reading original CSV files and merging into one CSV file
 
 df1 = pd.read_csv("file_a.csv")
 df2 = pd.read_csv("file_b.csv")
-
 df = pd.merge(df1, df2, on='user_id')
+#df.to_csv('merged_python.csv')
+df.columns = ['uid','email','first_name','last_name']
 
-df.to_csv('merged_python.csv')
+# Taking merged CSV file, comparing against Json response and adding Piano users IDs to replace customers IDs.
+
+r = requests.get('https://sandbox.tinypass.com/api/v3/publisher/user/search?api_token=zziNT81wShznajW2BD5eLA4VCkmNJ88Guye7Sw4D&aid=o1sRRZSLlw')
+x = r.json()
+api_df = json_normalize(x, 'users')
+
+
+dfa = df.set_index('email')
+dfa.update(api_df.set_index('email'))
+dfa.to_csv("merged_python_corrected.csv")
+
+
+
+
+
 
--- readme.md
+++ readme.md
@@ -3,10 +3,10 @@
 1. Clone / download respository into folder of your choice
 2. Create a virtual environment
 3. Activate virtual environmoent
-3. Type `Pip install requirements.txt`
+3. Type `Pip install -r requirements.txt`
 4. Type `cd piano_exercises`
 5. From terminal in same directory type `python python_exercise.py`
-6. Output from file_a.csv and file_b.csv will be merged into merged_python.csv
+6. Output from file_a.csv and file_b.csv will be merged into merged_python_corrected.csv
 7. From the terminal type `source bash_exercise.sh`
 8. Results from bash script will be output to merged_bash.csv
 
